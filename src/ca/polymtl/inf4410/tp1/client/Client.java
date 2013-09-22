8e716b0059733b5dfee174c48c1064e86464190e
unknown <gabriel.laprise@gmail.com>
2013-09-22 17:05:00 -0400

    ..

--- src/ca/polymtl/inf4410/tp1/client/Client.java
+++ src/ca/polymtl/inf4410/tp1/client/Client.java
@@ -15,6 +15,7 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
 
 import ca.polymtl.inf4410.tp1.shared.ServerInterface;
 import ca.polymtl.inf4410.tp1.shared.ServerInterface.RemoteFile;
